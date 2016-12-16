7377acbd9472d12ab3b80eaa47c8ed2a13f62983
dan-h-ch <chang.daniel@gmail.com>
2016-12-15 23:06:36 -0800

    (feat) got server up - having issue query from server

---
+++ server/config/routes.js
@@ -0,0 +1,53 @@
+var config = require('./config')
+var Parse = require('parse/node')
+
+module.exports = function(app, express){
+
+  // Parse query for rawdata - abstract out later
+  var getLocationData = function(appId, jsKey, serverURL, callback, error){
+
+    // $("#status-msg").text("Loading data from server...");
+
+    Parse.initialize(appId, jsKey);
+    Parse.serverURL = serverURL;
+
+    var DATA = [];
+    var skip = 0;
+    const limit = 1000;
+
+    var process = function(skip) {
+      var query = new Parse.Query("_User");
+      query.exists("location");
+      if (skip) {query.skip(skip);}
+      query.limit(limit);
+      return query.find().then(function(results){
+        DATA = DATA.concat(results);
+        skip += limit;
+        if (results.length == limit) {
+          process(skip);
+        } else {
+          callback(JSON.parse(JSON.stringify(DATA)));
+        }
+      }, function(err){
+        error(err.code + ", " + err.message);
+      })
+    }
+
+    process(skip);
+  }
+
+  app.get('/us/city_state', function(req, res) {
+    console.log('trying to get something')
+
+    getLocationData(config.appId, config.jsKey, config.serverURL, function(rawD){
+      console.log(rawD);
+      res.status(200).send(JSON.stringify(rawD));
+    }, function(error){
+      console.log(error);
+    });
+
+  })
+
+
+
+}
--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 var express = require("express")
 var app = express();
 var path = require("path")
-var db = require('../db/config');
+// var db = require('../db/config');
 var bodyParser = require('body-parser');
+var routes = require('./config/routes')
 
 // Parse JSON (uniform resource locators)
 app.use(bodyParser.json());
@@ -11,13 +12,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 var port = process.env.PORT || 1337;
 
+routes(app, express)
+
 app.listen(port, function() {
   console.log("listening on", port)
 })
 
 app.use(express.static('public'));
-
-app.get('/cityState', function(req, res) {
-  sendAllItem(req, res, req.params.userid);
-});
-
