15ee30e91a72786551aba302e098b96dc7321dd3
dan-h-ch <chang.daniel@gmail.com>
2016-12-16 18:08:39 -0800

    (feat) pie chart scaffolding in place

--- public/data.js
+++ public/data.js
@@ -180,6 +180,8 @@ var pointsWanted = 1000
 // create array that will be used for d3 plotting
 var genderArr = []
 
+var newGenderArr = []
+
 // fill the array
 for (var key in genderObj) {
   if (key === "Male" || key === "Female" || key === "LGBT") {
@@ -189,7 +191,12 @@ for (var key in genderObj) {
       genderArr.push({gender: key})
     }
   }
+  if (key === 'Male' || key ==='Female') {
+    newGenderArr.push({gender: key, count: genderObj[key]})
+  }
 }
+
+console.log(newGenderArr)
 
 // assigned an order so d3 can order the data - this is not actually needed since we populate one genderArr one gender at a time
 genderArr.forEach((entry) => {
@@ -204,20 +211,39 @@ genderArr.forEach((entry) => {
   }
 })
 
+// gender stuff
 var svg2 = d3.select("body")
   .append("svg")
   .attr("width", width2)
   .attr("height", height2);
 
-svg2.selectAll(".gender_dot")
-  .data(genderArr)
-  .enter().append("circle", ".gender_dot")
-  .sort(function(a,b) {
-    return a.genderOrder - b.genderOrder
-  })
-  .attr("r", 3)
-  .attr("cy", function(d, i) {return 4 + Math.floor(i/100) * 8})
-  .attr("cx", function(d, i) {return 4 + ((i % 100) * 8)})
+var genderMax = Math.max(newGenderArr[0].count, newGenderArr[1].count)
+var genderMin = Math.min(newGenderArr[0].count, newGenderArr[1].count)
+
+var genderSize = d3.scale.linear()
+  .domain([genderMin, genderMax])
+  .range([100,150])
+  .clamp(true);
+
+var svg3 = d3.select("body")
+  .append("svg")
+  .attr("width", width2)
+  .attr("height", height2);
+
+var genderSize3 = d3.scale.linear()
+  .domain([genderMin, genderMax])
+  .range([200,350])
+  .clamp(true);
+
+svg3.selectAll(".gender_image")
+  .data(newGenderArr)
+  .enter()
+  .append("svg:image", ".gender_image")
+  .attr("xlink:href", function(d) {return d.gender === "Male" ? "http://www.a-listinternational.com/wp-content/uploads/2016/06/brad-pitt-doesn-t-really-look-much-like-brad-pitt-in-these-photos-727400.jpg" : "https://s-media-cache-ak0.pinimg.com/736x/8f/3b/9c/8f3b9cda3783cf34a8354d7122e4a91c.jpg"})
+  .attr("width", function(d) {return genderSize3(d.count)})
+  .attr("height", function(d) {return genderSize3(d.count)})
+  .attr("y", function(d) {return height/2 - genderSize3(d.count)/2})
+  .attr("x", function(d, i) {return (i+1)*width/3 - genderSize3(d.count)/2})
   .attr("fill", function(d) {
     if (d.gender === 'Female') {
       return "pink"
@@ -229,4 +255,88 @@ svg2.selectAll(".gender_dot")
       return "gray"
     }
   })
+
+svg2.selectAll(".gender_dot")
+  .data(newGenderArr)
+  .enter()
+  .append("circle", ".gender_dot")
+  .attr("r", function(d) {return genderSize(d.count)})
+  .attr("cy", height/2)
+  .attr("cx", function(d, i) {return (i+1)*width/3})
+  .attr("fill", function(d) {
+    if (d.gender === 'Female') {
+      return "pink"
+    } else if (d.gender === 'Male') {
+      return "lightblue"
+    } else if (d.gender === 'LGBT') {
+      return "green"
+    } else {
+      return "gray"
+    }
+  })
+
+// svg2.selectAll(".gender_dot")
+//   .data(genderArr)
+//   .enter().append("circle", ".gender_dot")
+//   .sort(function(a,b) {
+//     return a.genderOrder - b.genderOrder
+//   })
+//   .attr("r", 3)
+//   .attr("cy", function(d, i) {return 4 + Math.floor(i/100) * 8})
+//   .attr("cx", function(d, i) {return 4 + ((i % 100) * 8)})
+//   .attr("fill", function(d) {
+//     if (d.gender === 'Female') {
+//       return "pink"
+//     } else if (d.gender === 'Male') {
+//       return "lightblue"
+//     } else if (d.gender === 'LGBT') {
+//       return "green"
+//     } else {
+//       return "gray"
+//     }
+//   })
+
+var width4 = 360;
+var height4 = 360;
+var radius = Math.min(width4, height4) /2
+
+var pieColor = d3.scale.ordinal()
+  .range(["pink", "lightblue"]);
+
+var svg4 = d3.select("body")
+  .append('svg')
+  .attr("width", width4)
+  .attr("height", height4)
+  .append('g')
+  .attr('transform', 'translate(' + (width4/2) + ',' + (height4/2) + ')')
+
+var arc = d3.svg.arc()
+  .outerRadius(radius - 10)
+  .innerRadius(0);
+
+var labelArc = d3.svg.arc()
+  .outerRadius(radius - 40)
+  .innerRadius(radius - 40);
+
+// converts data to data that can be used to make a pie chart
+var pie = d3.layout.pie()
+  .value(function(d) {return d.count})
+  .sort(null)
+
+console.log('newGenderArr', newGenderArr)
+
+console.log(pie(newGenderArr))
+
+var path = svg4.selectAll('path')
+  .data(pie(newGenderArr))
+  .enter()
+  .append('path')
+  .attr('d', arc)
+  .style("stroke", "silver")
+  .style("stroke-width", "3")
+  .attr('fill', function(d, i) {
+    return pieColor(d.data.gender)
+  })
+
+
 }
